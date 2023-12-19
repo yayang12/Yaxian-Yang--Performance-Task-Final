dd0fcc09f6b154eacbc28684cbe978ebd696096c
yayang12 <122055034+yayang12@users.noreply.github.com>
2023-12-19 11:20:08 -0800

    Sound effects complete

--- Player.gd
+++ Player.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 @export var speed = 400
+@onready var b_gmusic = $"../BGmusic"
+@onready var enemy_hit = $"../EnemyHit"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,3 +29,5 @@ func _process(delta):
 
 func _on_body_entered(body):
 	hide()
+	b_gmusic.stop()
+	enemy_hit.play()
